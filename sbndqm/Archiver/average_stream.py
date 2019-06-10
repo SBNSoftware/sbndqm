49e44ea1757c7ff9a321fbda00df6ae1f532eeef
Justin Mueller <mueller@sbnd-daq33.fnal.gov>
2019-06-10 11:28:16 -0500

    Added configuration file and more complexity to the average_stream.py

--- sbndqm/Archiver/average_stream.py
+++ sbndqm/Archiver/average_stream.py
@@ -1,24 +1,44 @@
 import argparse
 import redis
+import json
+#import numpy as np
 
 def main(args):
-    r = redis.Redis(host=args.hostname, port=args.port, password=args.password)
-    for stream in args.stream:
-        sum = 0
-        val = r.xrange(stream)
-        for item in val:
-            sum += float(item[1]["dat"])
-        print("Average: " + str(sum / len(val))) 
+    with open('ArchiverConfig.json') as json_file:
+        Config = json.load(json_file)
+    r = redis.Redis(host=Config['config']['hostname'], port=Config['config']['port'], password=args.password)
+    StreamDict = { x : Config['streams'][x]['latest'] for x in Config['streams'].keys()}
+    MetricDict = { x : [Config['streams'][x]['latest'], [] ] for x in Config['streams'].keys()}
+    print(StreamDict)
+    while True:
+        ReadStream = r.xread(StreamDict)
+        if len(ReadStream) !=0:
+            for StreamObject in ReadStream:
+                for DataObject in StreamObject[1]:
+                    if len(MetricDict[StreamObject[0]][1]) < Config['streams'][StreamObject[0]]:
+                        MetricDict[StreamObject[0]][1].append(float(DataObject[1]['dat']))
+                        MetricDict[StreamObject[0]][0] = DataObject[0]
+                    else:
+                        Config['streams'][StreamObject[0]]['latest'] = MetricDict[StreamObject[0]][0]
+                        StreamDict[StreamObject[0]] = MetricDict[StreamObject[0]][0]
+                        with open('ArchiverConfig.json', 'w') as OutputConfig:
+                            json.dump(Config, OutputConfig, indent=4, sort_keys=True)
+                        if Config['streams'][StreamObject[0]]['type'] == 'mean':
+                            print(str(StreamObject[0]) + "Mean: " + str(mean(MetricDict[StreamObject[0]][1])))
+                            MetricDict[StreamObject[0]][1] = []
+                        #elif Config['streams'][StreamObject[0]]['type'] == 'mode':
+                        #    print("Mode: " + str(np.mode(MetricDict[StreamObject[0]][1])))
+                        #    MetricDict[StreamObject[0]][1] = []
+                        #elif Config['streams'][StreamObject[0]]['type'] == 'median':
+                        #    print("Median: " + str(np.median(MetricDict[StreamObject[0]][1])))
+                        #    MetricDict[StreamObject[0]][1] = []
+                        MetricDict[StreamObject[0]].append(float(DataObject[1]['data']))
+                        MetricDict[StreamObject[0]][0] = DataObject[0]
 
-def comma_list(inp):
-    return [x for x in inp.split(",") if len(x) > 0]
+def mean(l):
+    return sum(l)/len(l)
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("-s", "--stream", required=True, type=comma_list)
-    args.add_argument("-t", "--timeout", default=1, type=float)
-    args.add_argument("-hn", "--hostname", default="localhost")
-    args.add_argument("-p", "--port", default=6379, type=int)
     args.add_argument("-pw", "--password", default=None)
-
     main(args.parse_args())
