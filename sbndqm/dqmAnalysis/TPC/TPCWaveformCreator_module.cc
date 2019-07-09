7d64ea81bd222f480314ce9bafe4cfb2736da5b6
Olivia Bitter <obitter@fnal.gov>
2019-07-09 11:45:36 -0500

    update waveform creator with redis stuff now!

--- sbndqm/dqmAnalysis/TPC/TPCWaveformCreator_module.cc
+++ sbndqm/dqmAnalysis/TPC/TPCWaveformCreator_module.cc
@@ -6,10 +6,12 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <TVirtualFFT.h>
 #include "TStopwatch.h"
 #include "TROOT.h"
 #include "TTree.h"
 #include "TH1F.h"
+#include "TH1D.h"
 
 #include "art/Framework/Core/EDAnalyzer.h"
 #include "art/Framework/Core/ModuleMacros.h"
@@ -25,6 +27,8 @@
 #include "ChannelData.hh"
 #include "HeaderData.hh"
 #include "Analysis.hh"
+
+#include "sbndaq-redis-plugin/Utilities.h"
 
 /* Uses the Analysis class to print stuff to file
 */
@@ -45,19 +49,34 @@ public:
    virtual void analyze(art::Event const & e) override;
 
 private:
-   tpcAnalysis::Analysis _analysis;
-   TTree *_output;
-   TStopwatch timer;
-   TStopwatch master;
+  tpcAnalysis::Analysis _analysis;
+  TTree *_output;
+  TH1D *Times;
+  TStopwatch timer;
+  TStopwatch master;
+  double rSum = 0.0;
+  double sSum = 0.0;
+  double Ped = 0.0;
+  double sum = 0.0;
+  double stringTime = 0.0;
+  double stringSum = 0.0;
+  redisContext* context;
+  double makeStrings(raw::RawDigit const&);
+  
 };
 
-void makeStrings(raw::RawDigit const& rd){ 
-   TStopwatch stringclock;
-   stringclock.Start();
-
+double tpcAnalysis::TPCWavefromCreator::makeStrings(raw::RawDigit const& rd){ 
+   TStopwatch sendString;
+   TStopwatch redisString;
+   
+   if (rd.Channel() == 0){
+     sSum = 0;
+     rSum =0;
+   }
+   sendString.Start();
    // store the waveform and fft's
    // also delete old lists
-   //redisAppendCommand(context, "DEL snapshot:waveform:wire:%i", rd.Channel());
+    redisAppendCommand(context, "DEL snapshot:waveform:wire:%i", rd.Channel());
 
    // we're gonna put the whole waveform into one very large list 
    // allocate enough space for it 
@@ -81,53 +100,75 @@ void makeStrings(raw::RawDigit const& rd){
    }
 
    // null terminate the string
-   //*buffer_index = '\0';
-   //redisAppendCommand(context, buffer);
-   //n_commands += 2;
-                                                                                                                           
+   *buffer_index = '\0';
+   redisAppendCommand(context, buffer);
+   sendString.Stop();
+   sSum = sSum + sendString.RealTime();
+   if (rd.Channel() == 575) {
+     std::cout<<" Time to read the buffer to redis is "<<sSum <<" seconds."<<std::endl;
+   }
+   redisString.Start();
+   redisGetReply(context,NULL);
+   redisGetReply(context,NULL);                                                                                                                         
    // delete the buffer                                                                                                                         
    delete buffer;
-
-  stringclock.Stop();
-  std::cout << "channel " << rd.Channel() << ": time for the string to be created is " << stringclock.RealTime()<< std::endl;
-  
+   redisString.Stop();
+   rSum = rSum + redisString.RealTime();
+   if (rd.Channel() == 575) {
+     std::cout<<" Time to receive the replies from  redis is "<<rSum <<" seconds."<<std::endl;
+   }
+   
+   if (rd.Channel() == 575) {
+     std::cout<<" Total time "<<sSum + rSum <<" seconds."<<std::endl;
+   }
+   double time = rSum + sSum;
+   
+   return time;
 }
 
 tpcAnalysis::TPCWavefromCreator::TPCWavefromCreator(fhicl::ParameterSet const & p):
   art::EDAnalyzer::EDAnalyzer(p),
-  _analysis(p){
-  master.Start();
+  _analysis(p){  
+  context =  sbndaq::Connect2Redis("icarus-db01",6379);//to make the configure options w/ password??  later 
+
+  art::ServiceHandle<art::TFileService> tfs; 
+  Times = tfs->make<TH1D>("Times","Channel_Times",100,0,2);   
+
 }
   
 void tpcAnalysis::TPCWavefromCreator::analyze(art::Event const & evt) {
+  master.Start();
   art::EventNumber_t eventNumber = evt.id().event();
   //get the raw digits from the event
   timer.Start();
   art::Handle<std::vector<raw::RawDigit>> raw_digits_handle;
   evt.getByLabel("daq", raw_digits_handle);
   timer.Stop();
-  std::cout << " time to read in raw digits " << timer.RealTime()<< std::endl;
+  //  std::cout << " Time to read in raw digits " << timer.RealTime()<< " seconds."<<std::endl;
    //wes prefers working with vectors, but you can use the handle/pointer if you want
   timer.Start(); 
   std::vector<raw::RawDigit> const& raw_digits_vector(*raw_digits_handle);
    // std::cout << "Size of vector is " << raw_digits_vector.size() << " or " << raw_digits_handle->size() << std::endl;
   timer.Stop();
-  std::cout << " time to read in vector " << timer.RealTime()<< std::endl;
+  // std::cout << " Time to read in vector " << timer.RealTime()<<" seconds."<< std::endl;
 
   art::ServiceHandle<art::TFileService> tfs; 
   std::vector<TH1F*> hist_vector_eventNumber;
-
+ 
    // std::cout <<  "Run " << evt.run() << ", subrun " << evt.subRun()
    //        << ", event " << eventNumber << " has " << raw_digits_handle->size()
    //	     << " channels"<< std::endl;
    timer.Start();
+ 
    for(size_t i_d=0; i_d<raw_digits_vector.size(); ++i_d){ 
      // using 'auto' here because I'm too lazy to type raw::RawDigit                                                                                  
      //raw::RawDigit const& rd = raw_digits_vector[i_d];
      auto const& rd = raw_digits_vector[i_d];
-     makeStrings(rd);
-   
-     //     std::cout << "Looking at " << i_d << " rawdigit. Channel number is ... " << rd.Channel() << std::endl;  
+     stringTime = makeStrings(rd);
+     // std::cout<<"Channel " << rd.Channel() << ": time for the string to be created is " <<stringTime<<" seconds."<<std::endl;
+     Times->Fill(stringTime);
+    
+     //std::cout << "Looking at " << i_d << " rawdigit. Channel number is ... " << rd.Channel() << std::endl;  
      std::stringstream ss_hist_title,ss_hist_name;
      ss_hist_title << "(Run,Event,rawdigit,Channel) "
     		   << evt.run() <<","
@@ -135,29 +176,92 @@ void tpcAnalysis::TPCWavefromCreator::analyze(art::Event const & evt) {
 		   <<i_d <<","
 		   <<rd.Channel();
 
-     ss_hist_name << "Waveform "
+     ss_hist_name << "Waveform_"
 	          <<eventNumber <<"_"
 	          <<i_d;
 
+
+     //FFT                                                                                                                                         
+
+     /*          int NDIM = 4096;
+     TVirtualFFT *fftr2c = TVirtualFFT::FFT(1, &NDIM, "R2C EX K");
+
+     //OK, let's try a Fourier transform now  
+     //first, we need a vector of doubles of the same size, to use as input                                       
+     std::vector<double> doubleVec(rd.ADCs().begin(),rd.ADCs().end());
+
+     std::cout << "Made double vec ... " << doubleVec.size() << std::endl;
+     //now run the FFT ... we already set it up!                                                                                                 
+     fftr2c->SetPoints(doubleVec.data());
+
+     fftr2c->Transform();
+
+
+     //make histograms FFT
+     TString h_name,h_title;
+     h_name.Form("h_re_%d_%d_%d",(int)(evt.id().event()),(int)((rd.Channel() & 0xff00) >> 8),(int)(rd.Channel() & 0xff));
+     h_title.Form("FFT, Real: Event %d, Board %d, Channel %d",
+		  (int)(evt.id().event()),(int)((rd.Channel() & 0xff00) >> 8),(int)(rd.Channel() & 0xff));
+     TH1 *hfft_re = 0;
+     hfft_re = TH1::TransformHisto(fftr2c,hfft_re,"RE");
+     hfft_re->SetName(h_name);
+     hfft_re->SetTitle(h_title);
+     hfft_re->GetXaxis()->SetRange(0,2049);
+      
+      
+     h_name.Form("h_im_%d_%d_%d",(int)(evt.id().event()),(int)((rd.Channel() & 0xff00) >> 8),(int)(rd.Channel() & 0xff));
+     h_title.Form("FFT, Imaginary: Event %d, Board %d, Channel %d",
+		  (int)(evt.id().event()),(int)((rd.Channel() & 0xff00) >> 8),(int)(rd.Channel() & 0xff));
+     TH1 *hfft_im = 0;
+     hfft_im = TH1::TransformHisto(fftr2c,hfft_im,"IM");
+     hfft_im->SetName(h_name);
+     hfft_im->SetTitle(h_title);
+     hfft_im->GetXaxis()->SetRange(0,2049);
+
+     h_name.Form("h_mag_%d_%d_%d",(int)(evt.id().event()),(int)((rd.Channel() & 0xff00) >> 8),(int)(rd.Channel() & 0xff));
+     h_title.Form("FFT, Magnitude: Event %d, Board %d, Channel %d",
+		  (int)(evt.id().event()),(int)((rd.Channel() & 0xff00) >> 8),(int)(rd.Channel() & 0xff));
+     TH1 *hfft_m = 0;
+     hfft_m = TH1::TransformHisto(fftr2c,hfft_m,"MAG");
+     hfft_m->SetName(h_name);
+     hfft_m->SetTitle(h_title);
+     hfft_m->GetXaxis()->SetRange(0,2049);
+
+     hfft_re->Write();
+     hfft_im->Write();
+     hfft_m->Write();
+
+     delete hfft_re;
+     delete hfft_im;
+     delete hfft_m;
+     
+     */
+     //make histograms Waveform      
      // std::cout << "going to create histogram " << ss_hist_name.str()<< std::endl;
 
 
-     hist_vector_eventNumber.push_back(tfs->make<TH1F>(ss_hist_name.str().c_str(),ss_hist_title.str().c_str(),2000,0,4096));
-     //     std::cout << "Created histo. Total histos is now  " << hist_vector_eventNumber.size()<< std::endl;
+     hist_vector_eventNumber.push_back(tfs->make<TH1F>(ss_hist_name.str().c_str(),ss_hist_title.str().c_str(),4096,0,4096));
+     //          std::cout << "Created histo. Total histos is now  " << hist_vector_eventNumber.size()<< std::endl;
      for(size_t i_t=0; i_t< rd.Samples(); ++i_t){
-       short my_adc_value = rd.ADC(i_t);  
+       short my_adc_value = rd.ADC(i_t); 
+       //       std::cout << " Wave val " << my_adc_value<<std::endl; 
        if(my_adc_value!=0) hist_vector_eventNumber.back()->SetBinContent(i_t+1,my_adc_value);
-    }
+
+     }
   }
 
    timer.Stop();
-   std::cout << " time to create histograms " << timer.RealTime()<< std::endl;
-   std::cout << " event " << eventNumber<< " is now completed." << std::endl;
+   std::cout << " Time to create three types of  histograms is " << timer.RealTime()<< " seconds."<< std::endl;
 
    master.Stop();
-   std::cout << " The total time for this event is   " << master.RealTime() 
-	     << " The overall total time accross all events is " << master.RealTime()*eventNumber << std::endl;
+   std::cout << " Event " << eventNumber<< " is now completed." << std::endl;
+   std::cout << " The total time for this event is " << master.RealTime()<<" seconds."<< std::endl;
+   sum = sum +master.RealTime();
+   std::cout  << " The overall total time accross all events is " << sum <<" seconds."<< std::endl;	    
+
+  
 }
+
 
 DEFINE_ART_MODULE(tpcAnalysis::TPCWavefromCreator)	 
 				  
