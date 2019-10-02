2894f3067e25ed8dfef97b0ed4a7f335b9937816
Olivia Bitter <obitter@fnal.gov>
2019-10-01 19:08:52 -0500

    updated radis

--- sbndqm/dqmAnalysis/TPC/TPCWaveformAndFftRedis_module.cc
+++ sbndqm/dqmAnalysis/TPC/TPCWaveformAndFftRedis_module.cc
@@ -32,8 +32,6 @@
 
 #include "sbndaq-redis-plugin/Utilities.h"
 
-/* Uses the Analysis class to print stuff to file
-*/
 namespace tpcAnalysis {
   class TPCWaveformAndFftRedis;
 }
@@ -41,8 +39,6 @@ namespace tpcAnalysis {
 class tpcAnalysis::TPCWaveformAndFftRedis : public art::EDAnalyzer {
 public:
   explicit TPCWaveformAndFftRedis(fhicl::ParameterSet const & p);
-  // The compiler-generated destructor is fine for non-base.
-  // classes without bare pointers or other resource use. Plugins should not be copied or assigned.
    TPCWaveformAndFftRedis(TPCWaveformAndFftRedis const &) = delete;
    TPCWaveformAndFftRedis(TPCWaveformAndFftRedis &&) = delete;
    TPCWaveformAndFftRedis & operator = (TPCWaveformAndFftRedis const &) = delete;
@@ -89,14 +85,12 @@ double tpcAnalysis::TPCWaveformAndFftRedis::makeFFT(raw::RawDigit const& rd,int 
     FSum = 0;
   }
    sendFFT.Start();
-  // store the waveform and also delete old lists                                                                                           
   redisAppendCommand(context, "DEL snapshot:fft:wire:%i", rd.Channel());
   auto val = rd.ADCs();
   for(size_t i_t=0; i_t< rd.Samples(); ++i_t){
     val[i_t] = rd.ADC(i_t) - Ped;
   }
   std::vector<double> doubleVec(val.begin(),val.end());
-  //now run the FFT ... we already set it up!                                                                                               
   fftr2c->SetPoints(doubleVec.data());
   fftr2c->Transform();
   TH1 *hfft_m = 0;
@@ -105,7 +99,6 @@ double tpcAnalysis::TPCWaveformAndFftRedis::makeFFT(raw::RawDigit const& rd,int 
   char *buffer = new char[buffer_len];  
   size_t print_len = sprintf(buffer, "RPUSH snapshot:fft:wire:%i", rd.Channel());
   char *buffer_index = buffer + print_len;
-  // throw in all of the data points                                                                                                            
   for(int  i_fft=1; i_fft<=hfft_m->GetNbinsX()*(.5); ++i_fft){
     float my_val = hfft_m->GetBinContent(i_fft);  
     print_len += sprintf(buffer_index, " %f",my_val);
@@ -115,23 +108,21 @@ double tpcAnalysis::TPCWaveformAndFftRedis::makeFFT(raw::RawDigit const& rd,int 
     std::exit(1);
      }
   }
-  // null terminate the string                            
   *buffer_index = '\0';
   redisAppendCommand(context, buffer);
   sendFFT.Stop();
    FSum = FSum + sendFFT.RealTime();
    if (rd.Channel() == 575) {
-   std::cout<<" Time to create the FFT for redis is "<<FSum <<" seconds."<<std::endl;
+     //   std::cout<<" Time to create the FFT for redis is "<<FSum <<" seconds."<<std::endl;
     }
   redisFFT.Start();
   redisGetReply(context,NULL);
   redisGetReply(context,NULL);
-  // delete the buffer                                                                                                                        
   delete buffer;
   redisFFT.Stop();
    fSum = fSum + redisFFT.RealTime();
    if (rd.Channel() == 575) {
-   std::cout<<" Time to send the  FFT to redis is "<<fSum <<" seconds."<<std::endl;
+     //std::cout<<" Time to send the  FFT to redis is "<<fSum <<" seconds."<<std::endl;
    }
    if (rd.Channel() == 575) {
   std::cout<<" Total time "<<fSum + FSum <<" seconds."<<std::endl;
@@ -143,25 +134,18 @@ double tpcAnalysis::TPCWaveformAndFftRedis::makeFFT(raw::RawDigit const& rd,int 
 
 double tpcAnalysis::TPCWaveformAndFftRedis::makeStrings(raw::RawDigit const& rd,int Ped){ 
    TStopwatch sendString;
-   TStopwatch redisString;
-   
+   TStopwatch redisString;   
    if (rd.Channel() == 0){
      sSum = 0;
      rSum = 0;
    }
    sendString.Start();
-   // store the waveform and also delete old lists
    redisAppendCommand(context, "DEL snapshot:waveform:wire:%i", rd.Channel());
-   // we're gonna put the whole waveform into one very large list 
-   // allocate enough space for it 
-   // Assume at max 4 chars per int plus a space each plus another 50 chars to store the base of the command
    auto waveform =  rd.ADCs();
    size_t buffer_len = waveform.size() * 10 + 50;
    char *buffer = new char[buffer_len];
-      // print in the base of the command
    size_t print_len = sprintf(buffer, "RPUSH snapshot:waveform:wire:%i", rd.Channel());
    char *buffer_index = buffer + print_len;
-   // throw in all of the data points
    for (int16_t dat: waveform) {
      print_len += sprintf(buffer_index, " %i", dat - Ped); 
      buffer_index = buffer + print_len;
@@ -170,13 +154,12 @@ double tpcAnalysis::TPCWaveformAndFftRedis::makeStrings(raw::RawDigit const& rd,
        std::exit(1);
      }
    }
-   // null terminate the string
    *buffer_index = '\0';
    redisAppendCommand(context, buffer);
    sendString.Stop();
    sSum = sSum + sendString.RealTime();
    if (rd.Channel() == 575) {
-     std::cout<<" Time to create the buffer for  redis is "<<sSum <<" seconds."<<std::endl;
+     // std::cout<<" Time to create the buffer for  redis is "<<sSum <<" seconds."<<std::endl;
    }
    redisString.Start();
    redisGetReply(context,NULL);
@@ -186,7 +169,7 @@ double tpcAnalysis::TPCWaveformAndFftRedis::makeStrings(raw::RawDigit const& rd,
    redisString.Stop();
    rSum = rSum + redisString.RealTime();
    if (rd.Channel() == 575) {
-     std::cout<<" Time to send the strings to redis is "<<rSum <<" seconds."<<std::endl;
+     // std::cout<<" Time to send the strings to redis is "<<rSum <<" seconds."<<std::endl;
    }
    if (rd.Channel() == 575) {
      std::cout<<" Total time "<<sSum + rSum <<" seconds."<<std::endl;
@@ -229,26 +212,21 @@ void tpcAnalysis::TPCWaveformAndFftRedis::analyze(art::Event const & evt) {
     }
   setup = setup + first.RealTime(); 
   art::EventNumber_t eventNumber = evt.id().event();
-  //get the raw digits from the event
   timer.Start();
   art::Handle<std::vector<raw::RawDigit>> raw_digits_handle;
   evt.getByLabel("daq", raw_digits_handle);
   timer.Stop();
-  //wes prefers working with vectors, but you can use the handle/pointer if you want
   timer.Start(); 
   std::vector<raw::RawDigit> const& raw_digits_vector(*raw_digits_handle);
-  // std::cout << "Size of vector is " << raw_digits_vector.size() << " or " << raw_digits_handle->size() << std::endl;
   timer.Stop();
-  // std::cout << " Time to read in vector " << timer.RealTime()<<" seconds."<< std::endl;
   art::ServiceHandle<art::TFileService> tfs; 
   std::vector<TH1F*> hist_vector_eventNumber;
    timer.Start();
    for(size_t i_d=0; i_d<raw_digits_vector.size(); ++i_d){ 
-     // using 'auto' here because I'm too lazy to type raw::RawDigit                                                                            
-     //raw::RawDigit const& rd = raw_digits_vector[i_d];
+     
      auto const& rd = raw_digits_vector[i_d];     
      
-      //loop to find sum of ADC values
+   
       for(size_t i_t=0; i_t< rd.Samples(); ++i_t){
 	if(i_t == 0) {
 	  counter = 1;
@@ -261,10 +239,10 @@ void tpcAnalysis::TPCWaveformAndFftRedis::analyze(art::Event const & evt) {
 	counter++;
        
       }
-      //loop for pedestal subtraction and storing that as the waveforms                                                                         
+      //loop for pedestal subtraction                                                                     
       for(size_t i_t=0; i_t< rd.Samples(); ++i_t){
 	Ped = Sum/(counter);
-       
+      
       
       }
 	
