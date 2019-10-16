8b783ccd0e3d442bb5a795227ff7293caec0b64b
Gray Putnam <grayputnam@uchicago.edu>
2019-10-16 09:58:27 -0500

    First draft of script to run larsoft jobs connecting to instances of the dispatcher.

---
+++ sbndqm/DAQConsumer/process.py
@@ -0,0 +1,69 @@
+import os
+import subprocess
+import tempfile
+import signal
+import select
+import threading
+import sys
+
+class ProcessFhiclException(Exception):
+    pass
+
+class ConsumerProcess(object):
+    def __init__(self, port, config_file_path, overwrite_path, output_file=None):
+        self.config_file_path = config_file_path
+        self.overwrite_path = overwrite_path
+        self.port = port
+        self.n_restart = 0
+        self.output_file = output_file
+        self.start()
+
+    def start(self):
+        try:
+            with open(self.config_file_path) as f:
+                text = f.read()
+        except:
+            raise ProcessFhiclException("Error: couldn't read fhicl file (%s)" % self.config_file_path)
+        self.temp_file = tempfile.NamedTemporaryFile() 
+        self.temp_file.write(text)
+        self.temp_file.write("\n\n%s: %i" % (self.overwrite_path, self.port))
+
+        self.process = None
+        # command = ["lar", "-c", new_config.name]     
+        # command = ["while", "sleep 1;", "ls", "-ltr"]
+        command = ["run_temp.sh"]
+        output_file = sys.stdout.fileno() if self.output_file is None else open(self.output_file, "w+")
+        try:
+            self.process = subprocess.Popen(command, stdout=output_file, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError, err:
+            self.retcode = err.code
+        output_file.close()
+
+    def cleanup(self):
+        pass
+        #if self.read_thread is not None:
+        #    self.read_thread.join()
+
+    def check_exit(self):
+        if self.process is None: return self.retcode
+        retcode = self.process.poll()
+        if retcode is not None:
+            self.process = None
+            self.retcode = retcode
+            return retcode
+        return None
+
+    def force_exit(self):
+        if self.process is None: return self.retcode
+        self.process.send_signal(signal.SIGINT)
+        return self.wait_exit()
+
+    def wait_exit(self):
+        if self.process is None: return self.retcode
+        self.retcode = self.process.wait()
+        self.process = None
+        return self.retcode
+
+    def restart(self):
+        self.n_restart += 1
+        self.start()
