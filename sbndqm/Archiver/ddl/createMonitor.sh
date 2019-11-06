9ffab827563a6bb14807ec47affc954c7278373f
William Badgett <badgett@icarus-gateway01.fnal.gov>
2019-11-06 10:09:21 -0600

    4th time is the charm

--- sbndqm/Archiver/ddl/createMonitor.sh
+++ sbndqm/Archiver/ddl/createMonitor.sh
@@ -13,5 +13,6 @@ if [ $# -ne 5 ]; then
 fi
 
 psql -U badgett -h cdpgsdev -p 5488 -d sbnteststand \
-   --set=GROUP_NAME=\'$1\' --set=METRIC=\'$2\' --set=MONITOR_TYPE=$3 -c "\i channelMonitor.sql"
+   --set=GROUP_NAME=\'$1\' --set=METRIC=\'$2\' --set=MONITOR_TYPE=$3 \
+   --set=CH1=$4 --set=CHN=$5 -c "\i channelMonitor.sql"
 
