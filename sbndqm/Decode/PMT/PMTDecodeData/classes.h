5bf8ea7eb6d37a9fb49191bcb75113546450ef87
Gennadiy <lukhanin@fnal.gov>
2021-06-23 19:46:53 -0500

    Fix class definition

--- sbndqm/Decode/PMT/PMTDecodeData/classes.h
+++ sbndqm/Decode/PMT/PMTDecodeData/classes.h
@@ -2,16 +2,16 @@
 #include <vector>
 #include "sbndqm/Decode/PMT/PMTDecodeData/PMTDigitizerInfo.hh"
 
-//namespace 
-//{
-//  
-//  struct dictionary 
-//  {
-//
-//    pmtAnalysis::PMTDigitizerInfo d;
-//    std::vector<pmtAnalysis::PMTDigitizerInfo> d_v;
-//    art::Wrapper<pmtAnalysis::PMTDigitizerInfo> d_w;
-//    art::Wrapper<std::vector<pmtAnalysis::PMTDigitizerInfo>> d_v_w;
-//  };
-//
-//}
+namespace 
+{
+  
+  struct dictionary 
+  {
+
+    pmtAnalysis::PMTDigitizerInfo d;
+    std::vector<pmtAnalysis::PMTDigitizerInfo> d_v;
+    art::Wrapper<pmtAnalysis::PMTDigitizerInfo> d_w;
+    art::Wrapper<std::vector<pmtAnalysis::PMTDigitizerInfo>> d_v_w;
+  };
+
+}
