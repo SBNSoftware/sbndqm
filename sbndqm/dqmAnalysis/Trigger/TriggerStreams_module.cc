0ffedc47b66b634576df6760b0fc2586b160e27b
Gennadiy <lukhanin@fnal.gov>
2021-10-29 05:53:26 -0500

    Basic working module for trigger streams

--- sbndqm/dqmAnalysis/Trigger/TriggerStreams_module.cc
+++ sbndqm/dqmAnalysis/Trigger/TriggerStreams_module.cc
@@ -21,6 +21,10 @@
 #include "sbndaq-online/helpers/Utilities.h"
 #include "sbndaq-online/helpers/EventMeta.h"
 
+#include "lardataobj/RawData/ExternalTrigger.h"
+#include "lardataobj/RawData/TriggerData.h"
+#include "lardataobj/Simulation/BeamGateInfo.h"
+
 #include <algorithm>
 #include <cassert>
 #include <stdio.h>
@@ -45,50 +49,27 @@ namespace sbndaq {
         virtual void analyze(art::Event const & evt);
   
     private:
-
-      const unsigned int nChannelsPerBoard = 16;
-      const unsigned int nBoards = 24;
-      unsigned int nTotalChannels = nBoards*nChannelsPerBoard;
-        
-        art::InputTag m_opdetwaveform_tag;
+    
+        art::InputTag m_trigger_tag;
 
         std::string m_redis_hostname;
-        
         int m_redis_port;
-
         double stringTime = 0.0;
 
         fhicl::ParameterSet m_metric_config;
 
-        pmtana::PulseRecoManager pulseRecoManager;
-        pmtana::PMTPulseRecoBase* threshAlg;
-        pmtana::PMTPedestalBase*  pedAlg;
-
- 
         void clean();
-
-        template<typename T> T Median( std::vector<T> data ) const ;
-
-        //int16_t Median(std::vector<int16_t> data, size_t n_adc);
-        
-        //double Median(std::vector<double> data, size_t n_adc);
-      
-        //double RMS(std::vector<int16_t> const& data, size_t n_adc, int16_t baseline ) const;
     
   };
 
-
 }
-
-
-
-
 
 sbndaq::TriggerStreams::TriggerStreams(fhicl::ParameterSet const & pset)
   : EDAnalyzer(pset)
-  , m_opdetwaveform_tag{ pset.get<art::InputTag>("OpDetWaveformLabel") }
+  , m_trigger_tag{ pset.get<art::InputTag>("TriggerLabel") }
   , m_redis_hostname{ pset.get<std::string>("RedisHostname", "icarus-db01") }
   , m_redis_port{ pset.get<int>("RedisPort", 6379) }
+  , m_metric_config{ pset.get<fhicl::ParameterSet>("TriggerMetricConfig") }
 {
 
   // Configure the redis metrics 
@@ -98,9 +79,7 @@ sbndaq::TriggerStreams::TriggerStreams(fhicl::ParameterSet const & pset)
 //------------------------------------------------------------------------------------------------------------------
 
 
-void sbndaq::TriggerStreams::clean() {
-
-}
+void sbndaq::TriggerStreams::clean() {}
 
 
 //------------------------------------------------------------------------------------------------------------------
@@ -108,52 +87,42 @@ void sbndaq::TriggerStreams::clean() {
 
 void sbndaq::TriggerStreams::analyze(art::Event const & evt) {
 
-  std::cout << "Printing something" << std::endl;
+  // Now we get the trigger information
+  art::Handle< std::vector<raw::Trigger> > triggerHandle;
+  evt.getByLabel( m_trigger_tag, triggerHandle );
 
-  //int level = 3; 
+  if( triggerHandle.isValid() && !triggerHandle->empty() ) {
+    std::cout << "OK" << std::endl;
+  }   
+  else {
+    mf::LogError("sbndaq::TriggerStreams::analyze") << "Data product raw::Trigger not found!\n";
+  }
 
-  //artdaq::MetricMode mode = artdaq::MetricMode::Average;
-  //artdaq::MetricMode rate = artdaq::MetricMode::Rate;
+  // Here we read the external trigger information
+  art::Handle< std::vector<raw::ExternalTrigger> > extTriggerHandle;
+  evt.getByLabel( m_trigger_tag, extTriggerHandle );
 
-  //std::string groupName = "Trigger";
+  if( extTriggerHandle.isValid() && !extTriggerHandle->empty() ) {
+    std::cout << "OK" << std::endl;
+  }
+  else {
+    mf::LogError("sbndaq::TriggerStreams::analyze") << "Data product raw::ExternalTrigger not found!\n";
+  }
+  
+  // Here we read the beam information
+  art::Handle< std::vector<sim::BeamGateInfo> > gateHandle;
+  evt.getByLabel( m_trigger_tag, gateHandle );
+
+  if( gateHandle.isValid() && !gateHandle->empty() ) {
+    std::cout << "OK" << std::endl;
+  }
+  else {
+    mf::LogError("sbndaq::TriggerStreams::analyze") << "Data product sim::GeteBeamInfo not found!\n";
+  }
 
 
-  // Now we look at the waveforms 
-  //art::Handle< std::vector<raw::OpDetWaveform> > opdetHandle;
-  //evt.getByLabel( m_opdetwaveform_tag, opdetHandle );
-
-  //if( opdetHandle.isValid() && !opdetHandle->empty() ) {
-
-      // Send the metrics 
-      //sbndaq::sendMetric(groupName, pmtId_s, "baseline", baseline, level, mode); // Send baseline information
-      //sbndaq::sendMetric(groupName, pmtId_s, "rms", rms, level, mode); // Send rms information
-      //sbndaq::sendMetric(groupName, pmtId_s, "rate", npulses, level, rate); // Send rms information
-        
-
-      // Now we send a copy of the waveforms 
-      //double tickPeriod = 0.002; // [us] 
-      //std::vector<std::vector<raw::ADC_Count_t>> adcs {opdetwaveform};
-      //std::vector<int> start { 0 }; // We are considreing each waveform independent for now 
-
-      //sbndaq::SendSplitWaveform("snapshot:waveform:PMT:" + pmtId_s, adcs, start, tickPeriod);
-      //sbndaq::SendEventMeta("snapshot:waveform:PMT:" + pmtId_s, evt);
-
-  //}
-
-//else {
-
-    //mf::LogError("sbndaq::CAENV1730Streams::analyze") 
-       //   << "Data product '" << m_opdetwaveform_tag.encode() << "' has no raw::OpDetWaveform in it!\n";
-
-     // }
  
 
-  // In case we need to clean something
-  //clean();
-
-  // Ronf for two seconds 
-  //sleep(2); // Is it still necessary ? uncomment if so
-   
 }
 
 DEFINE_ART_MODULE(sbndaq::TriggerStreams)
