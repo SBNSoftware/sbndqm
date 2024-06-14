6ca9603a77179f683bf5b7c984dc1a0cf5939db0
lynnt20 <lynn.tung20@gmail.com>
2024-06-14 14:29:29 -0500

    add features: - add function to determine sunset based on (Type B) blob waveform values - add calculation to the digital noise by calculating the neighbor differences - removes digital noise condition for adc values away from baseline (will say every sunset related wvfm is digi noise) - re-calculates baselines when running over non-sunset event

--- sbndqm/dqmAnalysis/SunsetAna/TPCSunsetAnalyzer_module.cc
+++ sbndqm/dqmAnalysis/SunsetAna/TPCSunsetAnalyzer_module.cc
@@ -20,6 +20,8 @@
 #include "lardataobj/RawData/RawDigit.h"
 #include "lardataobj/RawData/raw.h"
 
+#include "sbndqm/Decode/Mode/Mode.hh"
+
 #include <unordered_set>
 
 namespace sunsetAna {
@@ -44,6 +46,11 @@ public:
 
 private:
   std::unordered_set<raw::ChannelID_t> fDNChannels;   // set of channels with digital noise on them on this event
+  std::vector<float> fBaselines; // storage for baselines, needed to reference during sunset events
+
+  int fdefaultIndBaseline;
+  int fdefaultColBaseline;
+
   float fRMSCutWire;
   float fRMSCutRawDigit;
   int fNBADCutRawDigit;
@@ -52,7 +59,9 @@ private:
   int fNAwayFromPedestalRawDigit;
   int fDistFromPedestalRawDigit;
 
-  size_t GetDigiNoiseChannels(const std::vector<raw::RawDigit>& rawdigits);
+  bool   isSunsetEvent(const std::vector<raw::RawDigit>& rawdigits);
+  void   getBaselines(const std::vector<raw::RawDigit>& rawdigits, std::vector<float>& baselines);
+  size_t getDigiNoiseChannels(const std::vector<raw::RawDigit>& rawdigits);
 };
 
 
@@ -60,6 +69,8 @@ sunsetAna::TPCSunsetAnalyzer::TPCSunsetAnalyzer(fhicl::ParameterSet const& p)
   : EDAnalyzer{p}  // ,
   // More initializers here.
 {
+  fdefaultIndBaseline = p.get<int>("defaultIndBaseline",2048);
+  fdefaultColBaseline = p.get<int>("defaultColBaseline",460);
   // Call appropriate consumes<>() for any products to be retrieved by this module.
   fRMSCutWire = p.get<float>("RMSCutWire",100.0);
   fRMSCutRawDigit = p.get<float>("RMSCutRawDigit",100.0);
@@ -72,19 +83,62 @@ sunsetAna::TPCSunsetAnalyzer::TPCSunsetAnalyzer(fhicl::ParameterSet const& p)
 void sunsetAna::TPCSunsetAnalyzer::analyze(art::Event const& e)
 {
   fDNChannels.clear();
+  if (fBaselines.empty()){
+    fBaselines.resize(11264);
+    std::fill(fBaselines.begin(), fBaselines.begin()+3968, fdefaultIndBaseline);
+    std::fill(fBaselines.begin()+3968, fBaselines.begin()+5632, fdefaultColBaseline);
+    std::fill(fBaselines.begin()+5632, fBaselines.begin()+9000, fdefaultIndBaseline);
+    std::fill(fBaselines.begin()+9000, fBaselines.end(), fdefaultColBaseline);
+  }
 
   art::Handle digit_handle = e.getHandle<std::vector<raw::RawDigit>>("daq");
   if (digit_handle.isValid() && !digit_handle->empty()) {
     std::cout << "Found " << digit_handle->size() << " RawDigits." << std::endl;
-    auto ndigi_noise = GetDigiNoiseChannels(*digit_handle);
+    auto ndigi_noise = getDigiNoiseChannels(*digit_handle);
     std::cout << "Found " << ndigi_noise << " channels with digital noise." << std::endl;
+    bool isSunset = isSunsetEvent(*digit_handle);
+    if (isSunset==false){
+      getBaselines(*digit_handle, fBaselines);
+    }
   }
   else{
      std::cout << "No RawDigits found in this event." << std::endl;
   }
 }
 
-size_t sunsetAna::TPCSunsetAnalyzer::GetDigiNoiseChannels(const std::vector<raw::RawDigit>& rawdigits){
+bool sunsetAna::TPCSunsetAnalyzer::isSunsetEvent(const std::vector<raw::RawDigit>& rawdigits){
+  // look in the blob region, choose 6600:6700
+  // get the average max value in this region
+  float avg_maxval = 0;
+  auto nblob_ch = 0;
+
+  for (const auto& rd : rawdigits){
+    uint blob_min_ch = 6600;
+    uint blob_max_ch = 6700; 
+    auto chnum = rd.Channel();
+
+    if (chnum > blob_min_ch && chnum < blob_max_ch){
+      // ignore if this happens to be a digital noise channel
+      if (fDNChannels.find(chnum) != fDNChannels.end()) continue;
+
+      std::vector<short> rawadc;
+      rawadc.resize(rd.Samples());
+	    raw::Uncompress(rd.ADCs(), rawadc, rd.GetPedestal(), rd.Compression());
+      // get the max value in this region
+      short maxval = *std::max_element(rawadc.begin(), rawadc.end()) - fBaselines.at(chnum);
+      avg_maxval += maxval;
+      nblob_ch++;
+    }
+  }
+  if (nblob_ch) avg_maxval /= nblob_ch;
+  std::cout << "Average max value in blob region: " << avg_maxval << std::endl;
+  if (avg_maxval > 0)
+    return true;
+  else
+    return false;
+}
+
+size_t sunsetAna::TPCSunsetAnalyzer::getDigiNoiseChannels(const std::vector<raw::RawDigit>& rawdigits){
   for (const auto& rd : rawdigits){
 	  bool chanbad = false;
 	  chanbad |= (rd.GetSigma() > fRMSCutRawDigit);
@@ -94,21 +148,43 @@ size_t sunsetAna::TPCSunsetAnalyzer::GetDigiNoiseChannels(const std::vector<raw:
 	  raw::Uncompress(rd.ADCs(), rawadc, rd.GetPedestal(), rd.Compression());
 	  bool alleven = true;
 	  int naway = 0;
+    int pre_neighbor_diff = 0;
+    int post_neighbor_diff = 0;
 	  const short adc0 = rawadc.at(0);
+    // auto chnum = rd.Channel();
 	  for (size_t i=0; i< rd.Samples(); ++i)
 	    {
 	      const short adc = rawadc.at(i);
 	      if (adc == 0xBAD) ++nhexbad;
 	      alleven &= ( ((adc - adc0) % 2) == 0 );
-	      // if (std::abs(adc - rd.GetPedestal()) > fDistFromPedestalRawDigit){
-		    //   ++naway;
+        // ** this will make any sunset channels digital noise channels **
+	      // if (std::abs(adc - fBaselines.at(chnum)) > fDistFromPedestalRawDigit){
+		      // ++naway;
         // }
-	    }
+        if (i < rd.Samples()-1){
+          const short adc_next = rawadc.at(i+1);
+          if (i < 500)     pre_neighbor_diff += std::abs(adc - adc_next);
+          else if (i>1000) post_neighbor_diff += std::abs(adc - adc_next);
+        }
+      }
 	  chanbad |= (nhexbad > fNBADCutRawDigit);
 	  chanbad |= alleven;
 	  chanbad |= (naway > fNAwayFromPedestalRawDigit);
+    chanbad |= (pre_neighbor_diff > 5e4);
+    chanbad |= (post_neighbor_diff > 5e4);
 	  if (chanbad) fDNChannels.emplace(rd.Channel());
 	}
   return fDNChannels.size();
 }
+
+void sunsetAna::TPCSunsetAnalyzer::getBaselines(const std::vector<raw::RawDigit>& rawdigits, std::vector<float>& baselines){
+  for (const auto& rd : rawdigits){
+    auto chnum = rd.Channel();
+    std::vector<short> rawadc;
+    rawadc.resize(rd.Samples());
+    raw::Uncompress(rd.ADCs(), rawadc, rd.GetPedestal(), rd.Compression());
+    baselines.at(chnum) = Mode(rawadc, 10);
+  }
+}
+
 DEFINE_ART_MODULE(sunsetAna::TPCSunsetAnalyzer)
