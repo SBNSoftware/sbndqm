f722e029a8ff6da4d31c4b594a2bf72549bf085c
lynnt20 <lynn.tung20@gmail.com>
2025-01-10 10:48:19 -0600

    first attempt at getting hlts to differentiate between streams

--- sbndqm/dqmAnalysis/BeamMetrics/CAENV1730FlashMetricsSBND_module.cc
+++ sbndqm/dqmAnalysis/BeamMetrics/CAENV1730FlashMetricsSBND_module.cc
@@ -18,6 +18,11 @@
 #include "messagefacility/MessageLogger/MessageLogger.h"
 
 #include "sbndaq-artdaq-core/Obj/SBND/pmtSoftwareTrigger.hh"
+#include "sbndaq-artdaq-core/Overlays/SBND/PTBFragment.hh"
+#include "artdaq-core/Data/ContainerFragment.hh"
+#include "artdaq-core/Data/Fragment.hh"
+
+#include "sbndqm/dqmAnalysis/Utils/SBNDHLTFilterUtils.hh"
 
 #include "sbndqm/Decode/Mode/Mode.hh"
 #include "sbndaq-online/helpers/SBNMetricManager.h"
@@ -48,12 +53,35 @@ private:
 
   // Declare member data here.
   art::InputTag m_flashmetric_tag;
+
+  std::string   m_ptb_instance;
+  std::vector<uint64_t> m_HLT_beamzero;
+  std::vector<uint64_t> m_HLT_beamlight;
+  std::vector<uint64_t> m_HLT_beam_excluded;
+  std::vector<uint64_t> m_HLT_offbeamzero;
+  std::vector<uint64_t> m_HLT_offbeamlight;
+  std::vector<uint64_t> m_HLT_offbeam_excluded;
+  std::vector<uint64_t> m_HLT_crossingmuon;
+  std::vector<uint64_t> m_HLT_crossingmuon_excluded;
+
+  float         m_flashpeak_thresh;
+  int          getFileStream(art::Handle<std::vector<artdaq::Fragment> > ptb_handle);
 };
 
 
 CAENV1730FlashMetricsSBND::CAENV1730FlashMetricsSBND(fhicl::ParameterSet const& p)
   : EDAnalyzer{p}  // ,
   , m_flashmetric_tag{ p.get<art::InputTag>("FlashMetricLabel") }
+  , m_ptb_instance{ p.get<std::string>("PTBInstanceLabel","ContainerPTB") }
+  , m_HLT_beamzero{ p.get<std::vector<uint64_t>>("HLT_beamzero") }
+  , m_HLT_beamlight{ p.get<std::vector<uint64_t>>("HLT_beamlight") }
+  , m_HLT_beam_excluded{ p.get<std::vector<uint64_t>>("HLT_beam_excluded") }
+  , m_HLT_offbeamzero{ p.get<std::vector<uint64_t>>("HLT_offbeamzero") }
+  , m_HLT_offbeamlight{ p.get<std::vector<uint64_t>>("HLT_offbeamlight") }
+  , m_HLT_offbeam_excluded{ p.get<std::vector<uint64_t>>("HLT_offbeam_excluded") }
+  , m_HLT_crossingmuon{ p.get<std::vector<uint64_t>>("HLT_crossingmuon") }
+  , m_HLT_crossingmuon_excluded{ p.get<std::vector<uint64_t>>("HLT_crossingmuon_excluded") }
+  , m_flashpeak_thresh{ p.get<float>("FlashPeakThreshold",100) }
 {
   if (p.has_key("metrics")) {
     sbndaq::InitializeMetricManager(p.get<fhicl::ParameterSet>("metrics"));
@@ -65,6 +93,11 @@ void CAENV1730FlashMetricsSBND::analyze(art::Event const& e)
 {
   int level = 3;
 
+  art::Handle<std::vector<artdaq::Fragment> > ptb_handle;
+  e.getByLabel("daq", m_ptb_instance, ptb_handle);
+  auto stream = getFileStream(ptb_handle);
+  std::cout << "Stream: " << stream << std::endl;
+
   art::Handle pmtmetricHandle = e.getHandle<std::vector<sbnd::trigger::pmtSoftwareTrigger>>(m_flashmetric_tag);
   if( pmtmetricHandle.isValid() && !pmtmetricHandle->empty() ) {
     for (auto const & pmtmetric : *pmtmetricHandle) {
@@ -73,11 +106,47 @@ void CAENV1730FlashMetricsSBND::analyze(art::Event const& e)
       
       std::cout << "Flash ts: " << ts << " PE: " << pe << std::endl;
       if (pe>100){
-	sbndaq::sendMetric("BeamMetrics","0","flash_ts", ts, level, artdaq::MetricMode::LastPoint);
-	sbndaq::sendMetric("BeamMetrics","0","flash_pe", pe, level, artdaq::MetricMode::LastPoint);
+        sbndaq::sendMetric("BeamMetrics","0","flash_ts", ts, level, artdaq::MetricMode::LastPoint);
+        sbndaq::sendMetric("BeamMetrics","0","flash_pe", pe, level, artdaq::MetricMode::LastPoint);
       }
     }
   }
+}
+
+int CAENV1730FlashMetricsSBND::getFileStream(art::Handle<std::vector<artdaq::Fragment> > ptb_handle){
+  int stream = -1;
+  std::vector<uint64_t> hlt_vec;
+
+  for (auto const& cont : *ptb_handle){
+    std::cout << "Processing container" << std::endl;
+    artdaq::ContainerFragment contf(cont);
+    auto these_hlts = sbndqm::SBNDHLTFilterUtils::GetAllHLTs(&contf);
+    hlt_vec.insert(hlt_vec.end(), these_hlts.begin(), these_hlts.end());
+  }
+
+  std::cout << "HLT size = " << hlt_vec.size() << ", contains HLT = ";
+  for (auto const hlt: hlt_vec){
+    std::cout << hlt << " ";
+  }
+  bool passBeamZero = false;
+  bool passBeamLight = false;
+  bool passOffbeamZero = false;
+  bool passOffbeamLight = false;
+  bool passXmuon = false;
+
+  passBeamZero = sbndqm::SBNDHLTFilterUtils::ApplyGateFilter(hlt_vec, m_HLT_beamzero, m_HLT_beam_excluded);
+  passBeamLight= sbndqm::SBNDHLTFilterUtils::ApplyGateFilter(hlt_vec, m_HLT_beamlight, m_HLT_beam_excluded);
+  passOffbeamZero = sbndqm::SBNDHLTFilterUtils::ApplyGateFilter(hlt_vec, m_HLT_offbeamzero, m_HLT_offbeam_excluded);
+  passOffbeamLight = sbndqm::SBNDHLTFilterUtils::ApplyGateFilter(hlt_vec, m_HLT_offbeamlight, m_HLT_offbeam_excluded);
+  passXmuon   = sbndqm::SBNDHLTFilterUtils::ApplyGateFilter(hlt_vec, m_HLT_crossingmuon, m_HLT_crossingmuon_excluded);
+
+  if (passBeamZero) stream = 1;
+  if (passBeamLight) stream = 2;
+  if (passOffbeamZero) stream = 3;
+  if (passOffbeamLight) stream = 4;
+  if (passXmuon) stream = 5;
+
+  return stream;
 }
 
 DEFINE_ART_MODULE(CAENV1730FlashMetricsSBND)
