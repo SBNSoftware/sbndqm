c3bae9516479b632e3d49e5639eeab45caa8deb6
sbnd <sbnd@fnal.gov>
2025-06-27 16:01:23 -0500

    Adding password creation script

---
+++ fcl/generate_redis_password_fcl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Read password and strip newline
+REDIS_PASSWORD=$(tr -d '\n' < /home/nfs/sbnd/redis_passfile)
+
+# Do a safe substitution into the FHiCL
+sed "s|@REDIS_PASSWORD@|$REDIS_PASSWORD|g" redis_connection_template.fcl > redis_connection.fcl
