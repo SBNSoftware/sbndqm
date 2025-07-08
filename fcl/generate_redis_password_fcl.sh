162cef6c64ab666659389bd83a9ce973ab01496e
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2025-07-08 13:36:54 -0500

    split redis_connection into icarus and sbnd

--- fcl/generate_redis_password_fcl.sh
+++ fcl/generate_redis_password_fcl.sh
@@ -4,4 +4,4 @@
 REDIS_PASSWORD=$(tr -d '\n' < /home/nfs/sbnd/redis_passfile)
 
 # Do a safe substitution into the FHiCL
-sed "s|@REDIS_PASSWORD@|$REDIS_PASSWORD|g" redis_connection_template.fcl > redis_connection.fcl
+sed "s|@REDIS_PASSWORD@|$REDIS_PASSWORD|g" redis_connection_template.fcl > redis_connection_sbnd.fcl
